4fd4867c292b437b7b6bfda1f7aade4b44d525e8
andmigque <andmigque@gmail.com>
2025-03-16 21:13:31 -0400

    Add Get-FailedUnits, Wrapper around systemctl list-units Add Get-FailedUnits.Tests, Testing return of failed with array and success with empty

---
+++ App/Get/Get-FailedUnits.Tests.ps1
@@ -0,0 +1,76 @@
+Describe 'Get-FailedUnits' {
+    BeforeAll {
+        # Import the function under test
+        . $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+    }
+
+    Context 'When there are failed systemd units' {
+        It 'Should detect and list failed systemd units' {
+
+            # Mock systemctl to return JSON with a non-active unit
+            Mock -CommandName Get-FailedUnits -MockWith {
+                $Unit1 = @{
+                    Unit = 'proc-sys-fs-binfmt_misc.automount'
+                    Load = 'loaded'
+                    Active = 'failed'
+                    Sub = 'running'
+                    Description = 'Arbitrary Executable File Formats File System Automount Point'
+                }
+                $Unit2 = @{
+                    Unit = 'failed.service'
+                    Load = 'loaded'
+                    Active = 'inactive'
+                    Sub = 'running'
+                    Description = 'An inactive service'
+                }
+
+                return @($Unit1, $Unit2)
+            }
+
+            # Capture the output
+            $output = Get-FailedUnits
+
+            $first = $output | Select-Object -First 1
+            $last = $output | Select-Object -Last 1
+            
+            $first.Active | Should -BeIn @('failed', 'inactive')
+            $last.Active | Should -BeIn @('failed', 'inactive')
+
+
+        }
+    }
+
+    Context 'When all systemd units are active' {
+        It 'Should report no failed systemd units' {
+            # Mock systemctl to return JSON with a non-active unit
+            Mock -CommandName Get-FailedUnits -MockWith {
+                $Unit1 = @{
+                    Unit = 'proc-sys-fs-binfmt_misc.automount'
+                    Load = 'loaded'
+                    Active = 'active'
+                    Sub = 'running'
+                    Description = 'Arbitrary Executable File Formats File System Automount Point'
+                }
+                $Unit2 = @{
+                    Unit = 'failed.service'
+                    Load = 'loaded'
+                    Active = 'active'
+                    Sub = 'running'
+                    Description = 'An inactive service'
+                }
+
+                return @($Unit1, $Unit2)
+            }
+
+            # Capture the output
+            $output = Get-FailedUnits
+
+            $first = $output | Select-Object -First 1
+            $last = $output | Select-Object -Last 1
+            
+            $first.Active | Should -Not -BeIn @('failed', 'inactive')
+            $last.Active | Should -Not -BeIn @('failed', 'inactive')
+    
+        }
+    }
+}
