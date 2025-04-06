0be218695fa41edc593778a8b0066ed46a35f482
andmigque <andmigque@gmail.com>
2025-04-06 11:10:34 -0400

    Refactor Server.ps1, move all routes and pages to individual files Refactor chat.ps1, Integrate as chat page with LM Studio's API

--- Server.ps1
+++ Server.ps1
@@ -14,7 +14,6 @@ $Web = @{
     }
 }
 
-$CONTENT_JSON = 'application/json'
 
 Start-PodeServer -Threads 2 -EnablePool WebSockets {
     # Remove Import-Module here since we've already loaded it
@@ -26,7 +25,7 @@ Start-PodeServer -Threads 2 -EnablePool WebSockets {
     Use-PodeWebPages -Path ./pages
 
     Enable-PodeOpenApi -Title 'PowerNixx API' -Version 0.0.1
-
+    
     $PodeLogger = New-PodeLoggingMethod -Terminal 
     $PodeLogger | Enable-PodeErrorLogging -Levels Error, Informational, Verbose, Warning
     $PodeLogger | Enable-PodeRequestLogging
@@ -42,59 +41,13 @@ Start-PodeServer -Threads 2 -EnablePool WebSockets {
     $UserName = [System.Environment]::UserName
     $UserDomainName = [System.Environment]::UserDomainName
 
-    Add-PodeRoute -Method Get -Path '/llm/securerandom' -ScriptBlock {
-        $random1 = Get-SecureRandom
-        $random2 = Get-SecureRandom
-        Write-PodeJsonResponse -Value @{ 'random1' = $random1; 'random2' = $random2 }
-    }
-
-    Add-PodeRoute -Method Get -Path '/llm/cpuinfo' -ScriptBlock {
-        $unixCommand = Get-CpuStats | Select-Object -Property UserPercent, SystemPercent, UsagePercent
-        Write-PodeTextResponse -ContentType $CONTENT_JSON -Value $unixCommand
-        
-    }
-    
-    Add-PodeRoute -Method Get -Path '/llm/diskstats' -ScriptBlock {
-        $unixCommand = (Invoke-Expression 'jc /proc/diskstats') | Out-String
-        Write-PodeJsonResponse -ContentType $CONTENT_JSON -Value $unixCommand
-    }
-
-    Add-PodeRoute -Method Get -Path '/llm/meminfo' -ScriptBlock {
-        $unixCommand = (Invoke-Expression 'jc /proc/meminfo') | Out-String
-        Write-PodeJsonResponse -ContentType $CONTENT_JSON -Value $unixCommand
-    }   
-
-    Add-PodeRoute -Method Get -Path '/llm/crypto' -ScriptBlock {
-        $unixCommand = (Invoke-Expression 'jc /proc/crypto') | Out-String
-        Write-PodeJsonResponse -ContentType $CONTENT_JSON -Value $unixCommand
-    }
-
-    Add-PodeRoute -Method Get -Path '/llm/stats' -ScriptBlock {
-        $unixCommand = (Invoke-Expression 'jc /proc/stat') | Out-String
-        Write-PodeJsonResponse -ContentType $CONTENT_JSON -Value $unixCommand
-    }
-
-    Add-PodeRoute -Method Get -Path '/llm/uptime' -ScriptBlock {
-        $unixCommand = (Invoke-Expression 'jc /proc/uptime') | Out-String
-        Write-PodeJsonResponse -ContentType $CONTENT_JSON -Value $unixCommand
-    }
-
-    Add-PodeRoute -Method Get -Path '/llm/ioports' -ScriptBlock {
-        $unixCommand = (Invoke-Expression 'jc /proc/ioports') | Out-String
-        Write-PodeJsonResponse -ContentType $CONTENT_JSON -Value $unixCommand
-    }
-
-    Add-PodeRoute -Method Get -Path '/llm/version' -ScriptBlock {
-        $unixCommand = (Invoke-Expression 'jc /proc/version') | Out-String
-        Write-PodeJsonResponse -ContentType $CONTENT_JSON -Value $unixCommand
-    }
-
-    Add-PodeRoute -Method Get -Path '/llm/devices' -ScriptBlock {
-        $unixCommand = (Invoke-Expression 'jc /proc/devices') | Out-String
-        Write-PodeJsonResponse -ContentType $CONTENT_JSON -Value $unixCommand
-    }
 
     Set-PodeWebHomePage -NoTitle -DisplayName "$($UserName)@$($UserDomainName), $($OsVersion)" -Layouts @(
+        # $navAbout = New-PodeWebNavLink -Name 'About' -Url '/pages/About' -Icon 'help-circle'
+        # $navDiv = New-PodeWebNavDivider
+        # $navYT = New-PodeWebNavLink -Name 'YouTube' -Url 'https://youtube.com' -Icon 'youtube'
+
+        # Set-PodeWebNavDefault -Items $navAbout, $navDiv, $navYT
         New-PodeWebContainer -Content @(
             New-PodeWebChart -Name 'CPU Usage' -Height '30em' -Type Bar -AutoRefresh -RefreshInterval 3 -AsCard -ScriptBlock {
                 $stats = Get-CpuStats
@@ -110,7 +63,7 @@ Start-PodeServer -Threads 2 -EnablePool WebSockets {
                     }
                 } | ConvertTo-PodeWebChartData -LabelProperty Core -DatasetProperty @('Usage', 'System', 'User', 'IO')
             }
-            New-PodeWebChart -Name 'Memory Usage' -Colours '#3192d3' -Height '30em' -Type Bar -AutoRefresh -RefreshInterval 3 -AsCard -ScriptBlock {
+            New-PodeWebChart -Name 'Memory Usage' -Colours '#a416f8' -Height '30em' -Type Bar -AutoRefresh -RefreshInterval 3 -AsCard -ScriptBlock {
                 $memStats = Get-Memory
                 @(
                     @{
@@ -132,23 +85,5 @@ Start-PodeServer -Threads 2 -EnablePool WebSockets {
                 ) | ConvertTo-PodeWebChartData -LabelProperty Name -DatasetProperty Value
             }
         )
-    
-        Add-PodeWebPage -Name 'PowerShellGet' -Icon 'Settings' -Group 'Package' -ScriptBlock {
-            New-PodeWebCard -Content @(
-                New-PodeWebTable -Name 'PowerShellGet' -AutoRefresh -RefreshInterval 3 -Height '15em' -ScriptBlock {
-                    $Packages = Get-Package -AllVersions
-
-                    foreach ($Package in $Packages) {
-                        [ordered]@{
-                            Name     = $Package.Name
-                            Version  = $Package.Version
-                            Source   = $Package.Source
-                            Provider = $Package.Provider
-
-                        }
-                    }
-                }
-            )
-        }
     )
 }
--- pages/chat.ps1
+++ pages/chat.ps1
@@ -1,7 +1,54 @@
-Add-PodeWebPage -NewTab -NoTitle -Name 'Chat' -Icon 'Chip' -Group 'AI' -ScriptBlock {    
-    New-PodeWebIFrame -Name 'Chat' -CssClass 'header' -CssStyle @{Height = '70em' }  -Url 'http://localhost:7860'
-}
+# Add-PodeWebPage -NewTab -NoTitle -Name 'Chat' -Icon 'Chip' -Group 'AI' -ScriptBlock {    
+#     New-PodeWebIFrame -Name 'Chat' -CssClass 'header' -CssStyle @{Height = '80em' }  -Url 'http://localhost:7860'
+# }
 
-Add-PodeWebPage -NoTitle -Name 'WebSSH' -Icon 'Chip' -Group 'System' -ScriptBlock {
-    New-PodeWebIFrame -Name 'WebSSH'  -Url 'http://localhost:2222/ssh/host/127.0.0.1'
+# Add-PodeWebPage -NoTitle -Name 'WebSSH' -Icon 'Chip' -Group 'System' -ScriptBlock {
+#     New-PodeWebIFrame -Name 'WebSSH'  -Url 'http://localhost:2222/ssh/host/127.0.0.1'
+# }
+
+
+
+Add-PodeWebPage -Name 'Chat' -Icon 'Chat' -Group 'AI' -ScriptBlock {
+    New-PodeWebCodeEditor -Name 'CodeEditor' -Language 'Markdown'  -CssStyle @{Height = '35rem'} -Theme vs-dark
+    New-PodeWebLine
+    New-PodeWebForm -Name 'ChatForm' -ShowReset -ScriptBlock {
+        Reset-PodeWebForm -Name 'ChatForm'
+        # Define the LLM endpoint URL
+        $llmEndpoint = 'http://localhost:1234/v1/chat/completions'  # Replace with your actual endpoint
+        
+        # Get the message from the form data
+        $messageContent = $WebEvent.Data['ChatMessage']
+        $payload = @{
+            model    = 'gemma-3-12b-it'
+            messages = @(
+                @{
+                    role    = 'user'    
+                    content = $messageContent
+                }
+            )
+        } | ConvertTo-Json
+        
+        # Invoke the REST method to send the message to the LLM endpoint
+        try {
+            $response = Invoke-RestMethod -Uri $llmEndpoint -Method Post -Body $payload -ContentType 'application/json'
+            $choices = ${response}?.choices
+            $content = ${choices}[0]?.message?.content
+            if (-not [string]::IsNullOrEmpty(($content))) {
+$outMessageFormatted = "`
+User:`
+$($messageContent)`
+Assistant:`
+$($content)`
+"
+                #Out-PodeWebTextbox -Value $outMessageFormatted -Multiline -ReadOnly -Preformat -Size 20
+                Update-PodeWebCodeEditor -Name 'CodeEditor' -Value "$($outMessageFormatted)"
+            }
+        }
+        catch {
+            Show-PodeWebToast -Message "Error sending message: $($_.Exception.Message)"
+        }
+    } -Content @(
+        New-PodeWebTextbox -Id 'ChatMessage' -Name 'ChatMessage' -DisplayName 'Chat with Gemma' -CssStyle @{Height = '15em' } -Multiline -Placeholder 'Enter your message...' -AutoFocus -Required
+    
+    )
 }
