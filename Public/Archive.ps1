2c050d9eea2cf9016f94825f1087332d8f4de5c5
andmigque <andmigque@gmail.com>
2025-05-05 07:49:06 -0400

    Removed dead code from Archive.ps1

--- Public/Archive.ps1
+++ Public/Archive.ps1
@@ -10,59 +10,10 @@ function ConvertFrom-DirToTar {
     try {
         $ErrorActionPreference = 'Stop'
         [TarFile]::CreateFromDirectory($source, $TarFilePath, $true)
-        Write-Host "Tar file created successfully at: $filePath"
+        Write-Host "Tar file created successfully at: $TarFilePath"
     }
     catch {
         Write-Host "An error occurred: $($_.Exception.Message)"
         Write-Host $_.ScriptStackTrace
-    }
-}
-
-function ConvertFrom-DirToTarFiles {
-    param([string]$SrcDir, [string]$DestDir)
-
-    $source = (Resolve-Path -Path $SrcDir).Path
-    $destination = (Resolve-Path -Path $DestDir).Path
-
-    try {
-        Write-Host $source
-        Write-Host $destination
-
-        $files = Get-ChildItem -Path $source 
-
-        foreach ($file in $files) {
-
-            try {
-                $filePath = $file.FullName
-                $baseName = [IO.Path]::GetFileNameWithoutExtension($filePath)
-                $tarFilePath = "$($destination)$($baseName).tar"
-    
-                Write-Host $filePath
-                Write-Host $baseName
-                Write-Host $tarFilePath
-    
-                $fileStream = [System.IO.File]::OpenRead($filePath)
-                $tarStream = [System.IO.File]::Create($tarFilePath)
-                $tarWriter = [TarWriter]::new($tarStream, [TarEntryFormat]::Gnu, $true)
-                $tarEntry = [GnuTarEntry]::new('RegularFile', $baseName)
-                
-                $tarEntry.DataStream = $fileStream
-                $tarWriter.WriteEntry($tarEntry)
-            } catch {
-                Write-Information $_
-                Write-Information $_.Exception.StackTrace
-                Write-Information $_.ScriptStackTrace
-            } finally {
-                $tarWriter.Dispose()
-                $fileStream.Dispose()
-                $tarStream.Dispose()
-            }
-
-        }
-    }
-    catch {
-        Write-Error $_
-        Write-Error $_.Exception.StackTrace
-        Write-Error $_.ScriptStackTrace
     }
 }
