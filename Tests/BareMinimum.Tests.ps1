e1eb7fc2dea6354bd16a161a327b34cfa02194fd
andmigque <andmigque@gmail.com>
2025-05-05 23:43:12 -0400

    Remove dead code and renamed functions

--- Tests/BareMinimum.Tests.ps1
+++
@@ -1,8 +0,0 @@
-#Requires -Modules Pester
-function Local-TestFunc { return 'IT WORKS' }
-Describe 'Bare Minimum' {
-    It 'Should find local function' {
-        $result = Local-TestFunc -ErrorAction SilentlyContinue
-        $result | Should -Be 'IT WORKS'
-    }
-}  
