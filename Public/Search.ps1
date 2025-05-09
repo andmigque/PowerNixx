49609af97728cc89b357d7cf32838d4a5b4b2060
Andres Quesada <andmigque@gmail.com>
2025-05-09 19:10:44 -0400

    Fixed Parallel Search

--- Public/Search.ps1
+++ Public/Search.ps1
@@ -1,7 +1,7 @@
 using namespace System
 using namespace System.Collections
-
 Set-StrictMode -Version 3.0
+
 function Search-KeywordInFile {
     param (
         [Parameter(Mandatory = $true)]
@@ -10,80 +10,55 @@ function Search-KeywordInFile {
         [string] $SearchString
     )
 
-    # Check for mandatory parameters
     if ([string]::IsNullOrEmpty($Directory)) {
-        throw 'Error: Directory parameter cannot be null or empty.'
+        throw 'Error: Directory can not be empty.'
     }
     if ([string]::IsNullOrEmpty($SearchString)) {
-        throw "Error: SearchString parameter for directory '$Directory' cannot be null or empty."
+        throw 'Error: SearchString can not be empty.'
     }
 
     $Directory = Resolve-Path -Path $Directory
     $SearchString = $SearchString.Trim()
+    $startTime = Get-Date
 
-    
-    [ArrayList]$mutexFound = [ArrayList]::Synchronized(@())
-    [ArrayList]$mutexNotFound = [ArrayList]::Synchronized(@())
-    [ArrayList]$mutexFilesFound = [ArrayList]::Synchronized(@())
-    [ArrayList]$mutexFilenameFound = [ArrayList]::Synchronized(@())
+    Write-Progress -Activity '<--- File content search -->' -Status '!! Running !!'
 
-    $script:startTime = Get-Date
-
-    Write-Progress -Activity '<--- File content search --> ' -Status '!! Running !!'
-    $allItems = (Get-ChildItem -Path $Directory -Recurse -File -ErrorAction SilentlyContinue | ForEach-Object -Parallel {
-            $currentTime = Get-Date
-            $differential = ($currentTime - $using:startTime)
-            Write-Progress -Activity 'Searching' -Status "$differential ⛷"
-            $localSearchString = $using:SearchString
-            if ($localSearchString -eq $($_).FullName.ToLower().Trim()) {
-                $mutexFilenameFound += "$($_.FullName)"
-                $mutexFound += 1
+    $allItems = Get-ChildItem -Path $Directory -Recurse -File -ErrorAction SilentlyContinue | ForEach-Object -Parallel {
+        $localSearchString = $using:SearchString
+        $fullName = $_.FullName
+        
+        $found = $false
+        try {
+            if ($localSearchString -eq $_.FullName.ToLower().Trim()) {
+                $found = $true
             }
-            
-            if (Test-Path -Path $_.FullName -PathType Leaf -ErrorAction SilentlyContinue) {
-                if (Select-String -Path $_.FullName -Pattern $using:SearchString -Encoding utf8 -Quiet -ErrorAction SilentlyContinue) {
-                    $message = "Found $using:SearchString in $($_.FullName)"
-                    Write-Verbose $message
-                    $mutexFilesFound += "$($_.FullName)"
-                    $mutexFound += 1
-                    return @{
-                        Item  = $message
-                        Index = $mutexFound.Count
-                    }
-                }
-                else {
-                    $message = "$using:SearchString was not found in $($_.FullName)"
-                    $mutexNotFound += 1
-                    return @{
-                        Item  = $message
-                        Index = $mutexNotFound.Count
-                    }
+            elseif (Test-Path -Path $_.FullName -PathType Leaf -ErrorAction SilentlyContinue) {
+                if (Select-String -Path $_.FullName -Pattern $localSearchString -Encoding utf8 -Quiet -ErrorAction SilentlyContinue) {
+                    $found = $true
                 }
             }
-            else {
-                $message = "Testing path $($_.FullName) failed)"
-                $mutexNotFound += 1
-                return @{
-                    Item  = $message
-                    Index = $mutexNotFound.Count
-                }
-            }
-        })
+        }
+        catch {
+            # Optionally add error info
+        }
+        return @{
+            File  = $fullName
+            Found = $found
+        }
+    }
+
     Write-Progress -Activity '| File content search___⛷  ' -Status '!! Complete !!' -Completed
-    Write-Verbose ($allItems | Format-Table -AutoSize -RepeatHeader | Out-String)
 
+    $foundFiles = $allItems | Where-Object { $_.Found }
     $endTime = Get-Date
     $processingTime = ($endTime - $startTime).TotalSeconds
 
     $summary = [PSCustomObject]@{
-        'Scanned' = $($mutexFound.Count + $mutexNotFound.Count)
-        'Found'   = $mutexFound.Count
-        'Time'    = $processingTime
-        'Files'   = $mutexFilesFound
+        Scanned = $allItems.Count
+        Found   = $foundFiles.Count
+        Time    = $processingTime
+        Files   = $foundFiles.File
     }
 
-    Write-Verbose ($summary | Format-Table -AutoSize -RepeatHeader | Out-String -Width 100) 
-
-    return $summary
-    
+    $summary | ConvertTo-Json -Depth 3 -Compress
 }
