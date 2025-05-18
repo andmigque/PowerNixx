5e13ed7540d56d0782a1c48937bc465e4e6db72c
andmigque <andmigque@gmail.com>
2025-05-18 16:13:51 -0400

    Remove dead folders from .gitignore

---
+++ Public/PsNxSystem/PsNxSystem.psm1
@@ -0,0 +1,310 @@
+# Module created by Microsoft.PowerShell.Crescendo
+# Version: 1.1.0
+# Schema: https://aka.ms/PowerShell/Crescendo/Schemas/2021-11
+# Generated at: 05/18/2025 16:07:17
+class PowerShellCustomFunctionAttribute : System.Attribute {
+    [bool]$RequiresElevation
+    [string]$Source
+    PowerShellCustomFunctionAttribute() { $this.RequiresElevation = $false; $this.Source = "Microsoft.PowerShell.Crescendo" }
+    PowerShellCustomFunctionAttribute([bool]$rElevation) {
+        $this.RequiresElevation = $rElevation
+        $this.Source = "Microsoft.PowerShell.Crescendo"
+    }
+}
+
+# Returns available errors
+# Assumes that we are being called from within a script cmdlet when EmitAsError is used.
+function Pop-CrescendoNativeError {
+param ([switch]$EmitAsError)
+    while ($__CrescendoNativeErrorQueue.Count -gt 0) {
+        if ($EmitAsError) {
+            $msg = $__CrescendoNativeErrorQueue.Dequeue()
+            $er = [System.Management.Automation.ErrorRecord]::new([system.invalidoperationexception]::new($msg), $PSCmdlet.Name, "InvalidOperation", $msg)
+            $PSCmdlet.WriteError($er)
+        }
+        else {
+            $__CrescendoNativeErrorQueue.Dequeue()
+        }
+    }
+}
+# this is purposefully a filter rather than a function for streaming errors
+filter Push-CrescendoNativeError {
+    if ($_ -is [System.Management.Automation.ErrorRecord]) {
+        $__CrescendoNativeErrorQueue.Enqueue($_)
+    }
+    else {
+        $_
+    }
+}
+
+function Get-System
+{
+[PowerShellCustomFunctionAttribute(RequiresElevation=$False)]
+[CmdletBinding()]
+
+param(
+[Parameter()]
+[switch]$ListUnits,
+[Parameter()]
+[switch]$ListAutoMounts,
+[Parameter()]
+[switch]$ListSockets,
+[Parameter()]
+[switch]$ListTimers,
+[Parameter()]
+[switch]$Show,
+[Parameter()]
+[switch]$Status,
+[Parameter()]
+[switch]$All,
+[Parameter()]
+[switch]$Help,
+[Parameter()]
+[switch]$Now,
+[Parameter()]
+[switch]$Failed
+    )
+
+BEGIN {
+    $PSNativeCommandUseErrorActionPreference = $false
+    $__CrescendoNativeErrorQueue = [System.Collections.Queue]::new()
+    $__PARAMETERMAP = @{
+         ListUnits = @{
+               OriginalName = 'list-units'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+         ListAutoMounts = @{
+               OriginalName = 'list-automounts'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+         ListSockets = @{
+               OriginalName = 'list-sockets'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+         ListTimers = @{
+               OriginalName = 'list-timers'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+         Show = @{
+               OriginalName = 'show'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+         Status = @{
+               OriginalName = 'status'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+         All = @{
+               OriginalName = '--all'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+         Help = @{
+               OriginalName = '--help'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+         Now = @{
+               OriginalName = '--now'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+         Failed = @{
+               OriginalName = '--failed'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+    }
+
+    $__outputHandlers = @{ Default = @{ StreamOutput = $true; Handler = { $input; Pop-CrescendoNativeError -EmitAsError } } }
+}
+
+PROCESS {
+    $__boundParameters = $PSBoundParameters
+    $__defaultValueParameters = $PSCmdlet.MyInvocation.MyCommand.Parameters.Values.Where({$_.Attributes.Where({$_.TypeId.Name -eq "PSDefaultValueAttribute"})}).Name
+    $__defaultValueParameters.Where({ !$__boundParameters["$_"] }).ForEach({$__boundParameters["$_"] = get-variable -value $_})
+    $__commandArgs = @()
+    $MyInvocation.MyCommand.Parameters.Values.Where({$_.SwitchParameter -and $_.Name -notmatch "Debug|Whatif|Confirm|Verbose" -and ! $__boundParameters[$_.Name]}).ForEach({$__boundParameters[$_.Name] = [switch]::new($false)})
+    if ($__boundParameters["Debug"]){wait-debugger}
+    $__commandArgs += '--no-pager'
+    $__commandArgs += '--output=json'
+    foreach ($paramName in $__boundParameters.Keys|
+            Where-Object {!$__PARAMETERMAP[$_].ApplyToExecutable}|
+            Where-Object {!$__PARAMETERMAP[$_].ExcludeAsArgument}|
+            Sort-Object {$__PARAMETERMAP[$_].OriginalPosition}) {
+        $value = $__boundParameters[$paramName]
+        $param = $__PARAMETERMAP[$paramName]
+        if ($param) {
+            if ($value -is [switch]) {
+                 if ($value.IsPresent) {
+                     if ($param.OriginalName) { $__commandArgs += $param.OriginalName }
+                 }
+                 elseif ($param.DefaultMissingValue) { $__commandArgs += $param.DefaultMissingValue }
+            }
+            elseif ( $param.NoGap ) {
+                # if a transform is specified, use it and the construction of the values is up to the transform
+                if($param.ArgumentTransform -ne '$args') {
+                    $transform = $param.ArgumentTransform
+                    if($param.ArgumentTransformType -eq 'inline') {
+                        $transform = [scriptblock]::Create($param.ArgumentTransform)
+                    }
+                    $__commandArgs += & $transform $value
+                }
+                else {
+                    $pFmt = "{0}{1}"
+                    # quote the strings if they have spaces
+                    if($value -match "\s") { $pFmt = "{0}""{1}""" }
+                    $__commandArgs += $pFmt -f $param.OriginalName, $value
+                }
+            }
+            else {
+                if($param.OriginalName) { $__commandArgs += $param.OriginalName }
+                if($param.ArgumentTransformType -eq 'inline') {
+                   $transform = [scriptblock]::Create($param.ArgumentTransform)
+                }
+                else {
+                   $transform = $param.ArgumentTransform
+                }
+                $__commandArgs += & $transform $value
+            }
+        }
+    }
+    $__commandArgs = $__commandArgs | Where-Object {$_ -ne $null}
+    if ($__boundParameters["Debug"]){wait-debugger}
+    if ( $__boundParameters["Verbose"]) {
+         Write-Verbose -Verbose -Message "/usr/bin/systemctl"
+         $__commandArgs | Write-Verbose -Verbose
+    }
+    $__handlerInfo = $__outputHandlers[$PSCmdlet.ParameterSetName]
+    if (! $__handlerInfo ) {
+        $__handlerInfo = $__outputHandlers["Default"] # Guaranteed to be present
+    }
+    $__handler = $__handlerInfo.Handler
+    if ( $PSCmdlet.ShouldProcess("/usr/bin/systemctl $__commandArgs")) {
+    # check for the application and throw if it cannot be found
+        if ( -not (Get-Command -ErrorAction Ignore "/usr/bin/systemctl")) {
+          throw "Cannot find executable '/usr/bin/systemctl'"
+        }
+        if ( $__handlerInfo.StreamOutput ) {
+            if ( $null -eq $__handler ) {
+                & "/usr/bin/systemctl" $__commandArgs
+            }
+            else {
+                & "/usr/bin/systemctl" $__commandArgs 2>&1| Push-CrescendoNativeError | & $__handler
+            }
+        }
+        else {
+            $result = & "/usr/bin/systemctl" $__commandArgs 2>&1| Push-CrescendoNativeError
+            & $__handler $result
+        }
+    }
+    # be sure to let the user know if there are any errors
+    Pop-CrescendoNativeError -EmitAsError
+  } # end PROCESS
+
+<#
+
+
+.DESCRIPTION
+Wrapper for systemctl --no-pager --output=json
+
+.PARAMETER ListUnits
+List active systemctl units
+
+
+.PARAMETER ListAutoMounts
+List automount units currently in memory
+
+
+.PARAMETER ListSockets
+List socket units currently in memory
+
+
+.PARAMETER ListTimers
+List timer units currently in memory
+
+
+.PARAMETER Show
+Show properties of the system
+
+
+.PARAMETER Status
+The overall status or the status of the unit
+
+
+.PARAMETER All
+Give all output
+
+
+.PARAMETER Help
+Show help
+
+
+.PARAMETER Now
+Do the action now, dont wait for reload
+
+
+.PARAMETER Failed
+Any failed units
+
+
+
+#>
+}
+
+
---
+++ Public/PsNxTar/PsNxTar.psm1
@@ -0,0 +1,249 @@
+# Module created by Microsoft.PowerShell.Crescendo
+# Version: 1.1.0
+# Schema: https://aka.ms/PowerShell/Crescendo/Schemas/2021-11
+# Generated at: 05/18/2025 16:07:17
+class PowerShellCustomFunctionAttribute : System.Attribute {
+    [bool]$RequiresElevation
+    [string]$Source
+    PowerShellCustomFunctionAttribute() { $this.RequiresElevation = $false; $this.Source = "Microsoft.PowerShell.Crescendo" }
+    PowerShellCustomFunctionAttribute([bool]$rElevation) {
+        $this.RequiresElevation = $rElevation
+        $this.Source = "Microsoft.PowerShell.Crescendo"
+    }
+}
+
+# Returns available errors
+# Assumes that we are being called from within a script cmdlet when EmitAsError is used.
+function Pop-CrescendoNativeError {
+param ([switch]$EmitAsError)
+    while ($__CrescendoNativeErrorQueue.Count -gt 0) {
+        if ($EmitAsError) {
+            $msg = $__CrescendoNativeErrorQueue.Dequeue()
+            $er = [System.Management.Automation.ErrorRecord]::new([system.invalidoperationexception]::new($msg), $PSCmdlet.Name, "InvalidOperation", $msg)
+            $PSCmdlet.WriteError($er)
+        }
+        else {
+            $__CrescendoNativeErrorQueue.Dequeue()
+        }
+    }
+}
+# this is purposefully a filter rather than a function for streaming errors
+filter Push-CrescendoNativeError {
+    if ($_ -is [System.Management.Automation.ErrorRecord]) {
+        $__CrescendoNativeErrorQueue.Enqueue($_)
+    }
+    else {
+        $_
+    }
+}
+
+function Invoke-Tar
+{
+[PowerShellCustomFunctionAttribute(RequiresElevation=$False)]
+[CmdletBinding(SupportsShouldProcess=$true)]
+
+param(
+[Parameter()]
+[switch]$Show,
+[Parameter(Position=0,ValueFromPipeline=$true)]
+[string]$Archive,
+[Parameter(ParameterSetName='list')]
+[switch]$List,
+[Parameter(ParameterSetName='extract')]
+[switch]$Extract,
+[Parameter(ParameterSetName='Create')]
+[switch]$New,
+[Parameter(ParameterSetName='Create')]
+[string[]]$Files
+    )
+
+BEGIN {
+    $PSNativeCommandUseErrorActionPreference = $false
+    $__CrescendoNativeErrorQueue = [System.Collections.Queue]::new()
+    $__PARAMETERMAP = @{
+         Show = @{
+               OriginalName = '-v'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+         Archive = @{
+               OriginalName = '-f'
+               OriginalPosition = '0'
+               Position = '0'
+               ParameterType = 'string'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+         List = @{
+               OriginalName = '-t'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+         Extract = @{
+               OriginalName = '-x'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+         New = @{
+               OriginalName = '-c'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+         Files = @{
+               OriginalName = ''
+               OriginalPosition = '1'
+               Position = '2147483647'
+               ParameterType = 'string[]'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+    }
+
+    $__outputHandlers = @{ Default = @{ StreamOutput = $true; Handler = { $input; Pop-CrescendoNativeError -EmitAsError } } }
+}
+
+PROCESS {
+    $__boundParameters = $PSBoundParameters
+    $__defaultValueParameters = $PSCmdlet.MyInvocation.MyCommand.Parameters.Values.Where({$_.Attributes.Where({$_.TypeId.Name -eq "PSDefaultValueAttribute"})}).Name
+    $__defaultValueParameters.Where({ !$__boundParameters["$_"] }).ForEach({$__boundParameters["$_"] = get-variable -value $_})
+    $__commandArgs = @()
+    $MyInvocation.MyCommand.Parameters.Values.Where({$_.SwitchParameter -and $_.Name -notmatch "Debug|Whatif|Confirm|Verbose" -and ! $__boundParameters[$_.Name]}).ForEach({$__boundParameters[$_.Name] = [switch]::new($false)})
+    if ($__boundParameters["Debug"]){wait-debugger}
+    foreach ($paramName in $__boundParameters.Keys|
+            Where-Object {!$__PARAMETERMAP[$_].ApplyToExecutable}|
+            Where-Object {!$__PARAMETERMAP[$_].ExcludeAsArgument}|
+            Sort-Object {$__PARAMETERMAP[$_].OriginalPosition}) {
+        $value = $__boundParameters[$paramName]
+        $param = $__PARAMETERMAP[$paramName]
+        if ($param) {
+            if ($value -is [switch]) {
+                 if ($value.IsPresent) {
+                     if ($param.OriginalName) { $__commandArgs += $param.OriginalName }
+                 }
+                 elseif ($param.DefaultMissingValue) { $__commandArgs += $param.DefaultMissingValue }
+            }
+            elseif ( $param.NoGap ) {
+                # if a transform is specified, use it and the construction of the values is up to the transform
+                if($param.ArgumentTransform -ne '$args') {
+                    $transform = $param.ArgumentTransform
+                    if($param.ArgumentTransformType -eq 'inline') {
+                        $transform = [scriptblock]::Create($param.ArgumentTransform)
+                    }
+                    $__commandArgs += & $transform $value
+                }
+                else {
+                    $pFmt = "{0}{1}"
+                    # quote the strings if they have spaces
+                    if($value -match "\s") { $pFmt = "{0}""{1}""" }
+                    $__commandArgs += $pFmt -f $param.OriginalName, $value
+                }
+            }
+            else {
+                if($param.OriginalName) { $__commandArgs += $param.OriginalName }
+                if($param.ArgumentTransformType -eq 'inline') {
+                   $transform = [scriptblock]::Create($param.ArgumentTransform)
+                }
+                else {
+                   $transform = $param.ArgumentTransform
+                }
+                $__commandArgs += & $transform $value
+            }
+        }
+    }
+    $__commandArgs = $__commandArgs | Where-Object {$_ -ne $null}
+    if ($__boundParameters["Debug"]){wait-debugger}
+    if ( $__boundParameters["Verbose"]) {
+         Write-Verbose -Verbose -Message "/usr/bin/tar"
+         $__commandArgs | Write-Verbose -Verbose
+    }
+    $__handlerInfo = $__outputHandlers[$PSCmdlet.ParameterSetName]
+    if (! $__handlerInfo ) {
+        $__handlerInfo = $__outputHandlers["Default"] # Guaranteed to be present
+    }
+    $__handler = $__handlerInfo.Handler
+    if ( $PSCmdlet.ShouldProcess("/usr/bin/tar $__commandArgs")) {
+    # check for the application and throw if it cannot be found
+        if ( -not (Get-Command -ErrorAction Ignore "/usr/bin/tar")) {
+          throw "Cannot find executable '/usr/bin/tar'"
+        }
+        if ( $__handlerInfo.StreamOutput ) {
+            if ( $null -eq $__handler ) {
+                & "/usr/bin/tar" $__commandArgs
+            }
+            else {
+                & "/usr/bin/tar" $__commandArgs 2>&1| Push-CrescendoNativeError | & $__handler
+            }
+        }
+        else {
+            $result = & "/usr/bin/tar" $__commandArgs 2>&1| Push-CrescendoNativeError
+            & $__handler $result
+        }
+    }
+    # be sure to let the user know if there are any errors
+    Pop-CrescendoNativeError -EmitAsError
+  } # end PROCESS
+
+<#
+.SYNOPSIS
+tar wrapper
+
+.DESCRIPTION
+Just look at the man page
+
+.PARAMETER Show
+
+
+
+.PARAMETER Archive
+
+
+
+.PARAMETER List
+List the contents of an archive
+
+
+.PARAMETER Extract
+
+
+
+.PARAMETER New
+
+
+
+.PARAMETER Files
+
+
+
+
+.LINK
+link1
+link2
+
+#>
+}
+
+
