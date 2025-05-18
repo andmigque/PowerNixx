5b1d4573f8663cf7da80d04ead149234435b4af8
andmigque <andmigque@gmail.com>
2025-05-18 18:11:55 -0400

    Fixed VsCode type inference issue

--- Config/custom.profile.ps1
+++ Config/custom.profile.ps1
@@ -5,7 +5,7 @@ $PSStyle.OutputRendering = 'Ansi'
 # Disable PowerShell telemetry
 Set-Variable -Name 'POWERSHELL_TELEMETRY_OPTOUT' -Value 'true'	
 Set-Variable -Name 'PowerNixx' -Value "$env:HOME/Develop/PowerNixx"
-Set-Variable -Name 'ShowFastFetch' -Value 'false'
+Set-Variable -Name 'ShowFastFetch' -Value 'true'
 
 Import-Module $PowerNixx/PowerNixx.psd1 -Global -Force
 
@@ -20,11 +20,11 @@ if (Get-Module -Name PSReadLine -ListAvailable) {
 # Check if running on Linux
 if ($IsLinux) {
     # Initialize PATH environment variable
-    $env:PATH = ""
-    $env:PATH = "/opt/microsoft/powershell/7"
+    $env:PATH = ''
+    $env:PATH = '/opt/microsoft/powershell/7'
     
     # Add Homebrew paths if brew is installed
-    if (Test-Path "/home/linuxbrew/.linuxbrew/bin/brew") {
+    if (Test-Path '/home/linuxbrew/.linuxbrew/bin/brew') {
         $env:PATH = "$($env:PATH):/home/linuxbrew/.linuxbrew/sbin:/home/linuxbrew/.linuxbrew/bin"
     }
 
@@ -37,7 +37,7 @@ if ($IsLinux) {
     $env:PATH = "$($env:PATH):/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin"
     
     # Run fastfetch if available
-    if ((Test-Path "/usr/bin/fastfetch") -and ($ShowFastFetch -eq 'true')) {
+    if ((Test-Path '/usr/bin/fastfetch') -and ($ShowFastFetch -eq 'true')) {
         fastfetch
     }
 }
@@ -47,12 +47,12 @@ if ($IsLinux) {
 function prompt {
 
     # Get current git branch if in a git repository
-    $gitBranch = (Test-Path "./.git") ? ((Get-Content '.git/HEAD').replace('ref: refs/heads/','')) : '👎'
+    $gitBranch = (Test-Path './.git') ? ((Get-Content '.git/HEAD').replace('ref: refs/heads/', '')) : '👎'
     
     # Get current time in short format
     $shortTime = (Get-Date).ToShortTimeString()
 
     # Set prompt format with git branch and current time
-"`e[1m`e[94m╭PS`e[0m 🦾( ͡⚈ ʖ ͡⚈)🤳`e[1m`e[35m[$gitBranch]`e[0m`e[4m$($executionContext.SessionState.Path.CurrentLocation)`e[36m 🕒 $shortTime`e[0m`
-`e[94m╰┈➤`e[0m$(' ' * ($nestedPromptLevel + 1)) ";
+    "`e[1m`e[94m╭PS`e[0m 🦾( ͡⚈ ʖ ͡⚈)🤳`e[1m`e[35m[$gitBranch]`e[0m`e[4m$($executionContext.SessionState.Path.CurrentLocation)`e[36m 🕒 $shortTime`e[0m`
+`e[94m╰┈➤`e[0m$(' ' * ($nestedPromptLevel + 1)) "
 }
