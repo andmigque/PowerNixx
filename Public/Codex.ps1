e7f229cbe12cd19ad1b35b27dd980028003ec818
Andres Quesada <andmigque@gmail.com>
2025-09-15 22:12:15 -0400

    Adding in page to display codex lyricus. Adding in Codex.ps1 as rule scaffolding. Adding in Codex Lyricus.md

--- Public/Codex.ps1
+++ Public/Codex.ps1
@@ -1,11 +1,130 @@
-
-
 . (Join-Path -Path $PSScriptRoot -Resolve './PsNxEnums.ps1')
 . (Join-Path -Path $PSScriptRoot -Resolve './PsNxResult.ps1')
 
 class CodexRule {
     
-    static UniversalAndNovel(){
+    # 1. Universal Truth, Novel Expression
+    static UniversalTruthNovelExpression(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 2. Rhyme, DO IT
+    static RhymeDoIt(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 3. Clichés? No
+    static ClichesNo(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 4. Narrative Precision
+    static NarrativePrecision(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 5. Metaphors Multitask
+    static MetaphorsMultitask(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 6. Don’t be a V Chord
+    static DontBeAVChord(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 7. The Resonatrix
+    static TheResonatrix(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 8. Plain Admission
+    static PlainAdmission(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 9. Story Arc is Song Art
+    static StoryArcIsSongArt(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 10. Style Must Serve Substance
+    static StyleMustServeSubstance(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 15. Character Voice Must Stay Intact
+    static CharacterVoiceMustStayIntact(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 16. Every Word Must Earn Its Place
+    static EveryWordMustEarnItsPlace(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 17. Double Reality Check
+    static DoubleRealityCheck(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 18. Verse Length
+    static VerseLength(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 21. The Sensory Anchor
+    static TheSensoryAnchor(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 22. Editing as Law
+    static EditingAsLaw(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 23. Singability Resonates
+    static SingabilityResonates(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 24. Enjambment Exists, DO IT
+    static EnjambmentExistsDoIt(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 26. Alliteration Is Flavor, Not a Formula
+    static AlliterationIsFlavorNotAFormula(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 27. Emotional Hedging is Cowardice
+    static EmotionalHedgingIsCowardice(){
+        $Resultant = New-PsNxResult
+        Write-Host $Resultant
+    }
+
+    # 28. Every Section Needs a Centerpiece Line
+    static EverySectionNeedsACenterpieceLine(){
         $Resultant = New-PsNxResult
         Write-Host $Resultant
     }
